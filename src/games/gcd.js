290199f9a3210119c639d761276a30b64883358e
grepto <grepto@gmail.com>
2018-10-02 18:29:27 +0300

    gcd modile refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,8 +4,13 @@ import startGame from '../gameEngine';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.\n';
 
+const getAbsMin = (a, b) => {
+  if (Math.abs(a) <= Math.abs(b)) { return Math.abs(a); }
+  return Math.abs(b);
+};
+
 const getGcd = (a, b) => {
-  const minValue = Math.abs(a) <= Math.abs(b) ? Math.abs(a) : Math.abs(b);
+  const minValue = getAbsMin(a, b);
   for (let i = minValue; i > 0; i -= 1) {
     if (a % i === 0 && b % i === 0) return i;
   }
@@ -16,7 +21,6 @@ const pairGcd = () => {
   const a = randomInteger(1, 100);
   const b = randomInteger(1, 100);
   const answer = getGcd(a, b);
-  //  console.log(toString(cons(`${a} ${b}`, answer)));
   return cons(`${a} ${b}`, answer);
 };
 
