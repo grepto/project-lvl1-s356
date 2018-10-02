38d3e094d54de729c8d914e0b785b0ff3ae88f94
grepto <grepto@gmail.com>
2018-10-02 18:20:33 +0300

    even modile refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -4,12 +4,13 @@ import startGame from '../gameEngine';
 
 const gameDescription = 'Answer "yes" if number even otherwise answer "no".\n';
 
-const pairEven = () => {
-  const a = randomInteger(1, 100);
-  const answer = a % 2 === 0 ? 'yes' : 'no';
+const isEven = question => question % 2 === 0;
 
-  // console.log(toString(cons(a, answer)));
-  return cons(a, answer);
+const pairEven = () => {
+  const question = randomInteger(1, 100);
+  const answer = isEven(question) ? 'yes' : 'no';
+
+  return cons(question, answer);
 };
 
 const gameEven = () => startGame(pairEven, gameDescription);
